3f54ce74b0177c1632effa921c23fded8b0a42af
jcisking <krisloeffler@gmail.com>
2024-02-03 19:57:45 -0700

    modified LemonadeImageAndInstruction sig and fixed typo in string res

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -3,29 +3,17 @@ package com.example.lemonade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,6 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    LemonadeApp()
 
                 }
             }
@@ -45,19 +34,22 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun LemonadeApp() {
+    LemonadeImageAndInstruction()
+
+}
 
 @Composable
 fun LemonadeImageAndInstruction(
-    @DrawableRes imageRes: Int,
-    @StringRes instructionRes: Int,
     modifier: Modifier = Modifier
 ) {
     Column(modifier = modifier) {
         Image(
-            painter = painterResource(id = imageRes), 
+            painter = painterResource(id = R.drawable.lemon_drink),
             contentDescription = null
         )
-        Text(text = stringResource(id = instructionRes))
+        Text(text = stringResource(id = R.string.full_glass_string))
     }
 }
 
