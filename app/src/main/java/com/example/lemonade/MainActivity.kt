fb7f56c7300e62e1a5ab8f621f74130af462880c
jcisking <krisloeffler@gmail.com>
2024-02-04 19:47:47 -0700

    modified LemonadeImageAndInstruction and added LemonadeTopAppBar

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -4,16 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,29 +48,45 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun LemonadeApp() {
-    LemonadeImageAndInstruction()
 
+@Composable
+@Preview
+fun LemonadeApp() {
+    LemonadeTopAppBar()
+    LemonadeImageAndInstruction(
+        modifier = Modifier
+        .wrapContentSize()
+    )
+
+}
+
+@Composable
+fun LemonadeTopAppBar() {
+    Box(
+        modifier = Modifier.background(Color.Yellow)
+    ) {
+        Text(text = stringResource(id = R.string.top_app_bar))
+    }
 }
 
 @Composable
 fun LemonadeImageAndInstruction(
     modifier: Modifier = Modifier
 ) {
-    Column(modifier = modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
         Image(
             painter = painterResource(id = R.drawable.lemon_drink),
+            modifier = Modifier
+                .padding(bottom = 24.dp)
+                .background(Color.Green, RoundedCornerShape(100f))
+                .padding(40.dp),
             contentDescription = null
         )
         Text(text = stringResource(id = R.string.full_glass_string))
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LemonadeTheme {
-
-    }
-}
